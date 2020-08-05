7217ca2e31b023030e2be66886be4ecadb22afa1
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-08-05 09:00:34 +0200

    Alternative approach to challenge 2

--- 02-word-count-challenge.exs
+++ 02-word-count-challenge.exs
@@ -1,17 +1,27 @@
-resp = IO.gets "What would you like to count? Words, characters or lines in a file (type w/c/l):" |> String.trim()
-IO.puts resp
-case resp do
-    "w" -> IO.puts "Count words"
-    "c" -> IO.puts "Count characters"
-    "l" -> IO.puts "count lines"
-    _ -> 
-end
+# Part 1: Get file and format input
+filename = IO.gets("File to apply counter to: ") |> String.trim()
 
-filename = IO.gets("File to count the word from: ") |> String.trim()
-
-words= File.read!(filename)
+fileContent = File.read!(filename)
+words = fileContent
     |> String.split(~r{(\\n|[^\w'])+})
     |> Enum.filter(fn x -> x != "" end)
+chars = String.to_charlist(fileContent)
+lines = String.split(fileContent,~r{(\r\n|\n|\r)})
 
-IO.inspect words
-words |> Enum.count() |> IO.puts()
+# Part 2: Ask user of wanted output
+userRequests =  IO.gets("""
+What would you like to count? Words, characters or lines in dinwl?
+Type w, c or l - or combinations of it:
+""") |> String.trim() |> String.to_charlist()
+
+Enum.each(userRequests,fn x ->
+    case x do
+        119 -> # 'w'
+            IO.puts "Words: #{Enum.count(words)}"
+        99 -> # 'c'
+             IO.puts "Characters: #{length(chars)}"
+        108 -> # 'l'
+            IO.puts "Lines: #{Enum.count(lines)}"
+        _ -> IO.puts ~s{Requested option "#{[x]}" does not exist}
+    end
+end)
