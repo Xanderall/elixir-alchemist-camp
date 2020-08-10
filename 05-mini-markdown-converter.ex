8b6d938f6f6c9406ce774a48db066059842436f8
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-08-10 15:32:32 +0200

    regex deep dive

--- 05-mini-markdown-converter.ex
+++ 05-mini-markdown-converter.ex
@@ -1,9 +1,31 @@
 defmodule MiniMarkdown do
   
+  # Try it out with:
+  # test_string |> to_html
+  # write_test
+
   def to_html(text) do
     text
+      |> h2
+      |> h1
+      |> p
       |> bold
       |> italics
+      |> small
+      |> big
+  end
+
+  def h2(text) do
+    Regex.replace(~r/(\## +)([^\r\n]+)((\r\n|\r|\n)+$)?/, text, "<h2>\\2</h2>")
+  end
+
+  def h1(text) do
+    Regex.replace(~r/(\# +)([^\r\n]+)((\r\n|\r|\n)+$)?/, text, "<h1>\\2</h1>")
+    #Regex.replace(~r/(\r\n|\r|\n|^)\# +([^#][^\n\r]+)/, text, "<h1>\\2</h1>")
+  end
+
+  def p(text) do
+    Regex.replace(~r/(\r\n|\r|\n|^)+([^#<][^\r\n]+)((\r\n|\r|\n)+$)?/, text, "<p>\\2</p>")
   end
 
   def bold(text) do
@@ -12,6 +34,34 @@ defmodule MiniMarkdown do
 
   def italics(text) do
     Regex.replace(~r/\*(.*)\*/, text, "<em>\\1</em>")
+  end
+
+  def small(text) do
+    Regex.replace(~r/\-\-(.*)\-\-/, text, "<small>\\1</small>")
+  end
+
+  def big(text) do
+    Regex.replace(~r/\+\+(.*)\+\+/, text, "<big>\\1</big>")
+  end
+
+  def test_string do
+    """
+    # The essence of markdown coding
+    I *so much* enjoyed the sun outside, that I was in an **excellent mood** for coding.
+    
+    ## Your opinion counts
+    What about you?
+
+    Have fun with ++big++ and --small-- surprises!
+    """
+  end
+
+  def write_test() do
+    File.write("05-markdown-result.html", test_string |> to_html)
+  end
+
+  def easy_replace(text, str, tag) do #an idea to streamline
+    Regex.replace(~r/#{str}(.*)#{str}/, text, "<#{tag}>\\1</#{tag}>")
   end
 
 end
