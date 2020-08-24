ec17a89a56ba82ac3896e1d7303b202f9c16de1e
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-08-24 18:33:06 +0200

    bits & binaries deep dive

--- 12-bitmap-file-generator-with-color-palette.ex
+++ 12-bitmap-file-generator-with-color-palette.ex
@@ -5,9 +5,43 @@ defmodule BMP do
 
 # try generate_file()
 
+  # 1. Generate example picture without color palette (using standard 24 bit one)
   def generate_file(width \\ 30, height \\ 47, name \\ "12-generated-pic.bmp") do
     save(name, file_header() <> win2x_header(width, height), generate_example_data(width, height))
   end
+
+  # 2. Generate example picture with monochrome color palette (1 bit per pixel)
+  def generate_monochrome(width \\ 500, height \\ 500, name \\ "12-generated-monochrome-pic.bmp") do
+    header = file_header(26 + 3 * 2) # two colors with 3 bytes each
+             <> win2x_header(width, height, 1) # 1 bit per pixel
+    palette = win2x_palette([[255,0,128],[127,255,127]])
+    data = for row <- 1..height, into: <<>> do
+        cols = for col <- 1..width, into: <<>> do
+          <<(if row*row + col*col > 100_000, do: 1, else: 0)::1>>
+        end
+        <<cols::bitstring, padding_for(width, 1)::bitstring>> # 1 bit / pixel
+    end
+    save(name, header <> palette, data)
+  end
+
+  # 3. Generate example picture with 4-bit color palette (4 bits per pixel > makes 16 colors)
+  def generate_4bit(width \\ 400, height \\ 100, name \\ "12-generated-4bit-pic.bmp") do
+    header = file_header(26 + 3 * 16) <> win2x_header(width, height, 4) # 4 bits per pixel
+    # colors = [[255,0,128],[127,255,127],[12,25,127],[12,255,127],[127,25,127],[127,255,17],[127,25,127]]...
+    # use 16 random colors instead of fixed palette
+    randomColors = for _ <- 1..16, into: [], do: rPix()
+    palette = win2x_palette(randomColors)
+    data = for _row <- 1..height, into: <<>> do
+        cols = for col <- 1..width, into: <<>> do
+          <<rem(div(col-1, 25), 16)::4>>
+        end
+        <<cols::bitstring, padding_for(width, 4)::bitstring>> # 4 bits / pixel
+    end
+    save(name, header <> palette, data)
+  end
+
+  def rNbr, do: Enum.random(0..255)
+  def rPix, do: [rNbr(), rNbr(), rNbr()]
 
 # BMP file Header
 # typedef struct _WinBMPFileHeader
@@ -50,11 +84,9 @@ defmodule BMP do
 
   def generate_example_data(width, height) do
     for row <- 1..height, into: <<>> do
-        bytes_past = rem(3 * width, 4)
-        padding = (4 - bytes_past) * 8
         for item <- 1..width, into: <<>> do
           pixel(100 + 5 * item, 2 * row, 5 * item + row)
-        end <> <<0::size(padding)>>
+        end <> padding_for(width, 24) #24 bits / pixel
     end
   end
 
@@ -62,11 +94,17 @@ defmodule BMP do
     <<blue::little-8, green::little-8, red::little-8>>
   end
 
+  def padding_for(width, bpp \\ 24) do
+    bits_past = rem(width * bpp, 32)
+    num = if bits_past>0, do: (32 - bits_past), else: 0
+    <<0::size(num)>>
+  end
+
   def save(filename, header, pixels) do
     File.write!(filename, header <> pixels)
   end
 
-  def win2x_palette do
-    
+  def win2x_palette(colors) do
+    Enum.into(colors, <<>>, &(apply(BMP, :pixel, &1)))
   end
 end
