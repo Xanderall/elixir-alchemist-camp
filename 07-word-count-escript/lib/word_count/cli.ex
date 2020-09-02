78b7c40dd24b2eed4f2cdd7335a3063d6a9969f3
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-09-02 12:49:03 +0200

    cli executables with escript

---
+++ 07-word-count-escript/lib/word_count/cli.ex
@@ -0,0 +1,10 @@
+defmodule WordCount.CLI do
+  def main(args) do
+    {parsed, arg, invalid} = OptionParser.parse(
+        args,
+        switches: [chars: nil, lines: nil, words: nil], #what should be returned
+        aliases: [c: :chars, l: :lines, w: :words] # shortcut-alignment
+    )
+    IO.inspect {parsed, arg, invalid}
+  end
+end
