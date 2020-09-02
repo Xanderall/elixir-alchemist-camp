d9e727200dc9f98dd852383f1ea9ac5894704b26
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-09-02 13:35:35 +0200

    min executable program built

--- 07-word-count-escript/lib/word_count.ex
+++ 07-word-count-escript/lib/word_count.ex
@@ -1,18 +1,53 @@
 defmodule WordCount do
-  @moduledoc """
-  Documentation for `WordCount`.
-  """
 
-  @doc """
-  Hello world.
+  def start(parsed, file, invalid) do
+    if (invalid != []) || (file == "h") do
+      show_help()
+    else
+      read_file(parsed, file)
+    end
+  end
 
-  ## Examples
+  def show_help() do
+    IO.puts """
+What would you like to count? Words, characters or lines?
 
-      iex> WordCount.hello()
-      :world
+Usage: [filename] -[flags]
+For flags type (w)ords, (c)haracters or (l)ines - or combinations of it:
 
-  """
-  def hello do
-    :world
+E.g.: somefile.txt -lc
+"""
+  end
+
+  def read_file(parsed, filename) do
+
+    # Interprete flags given by user via cli
+    flags = case Enum.count(parsed) do
+      0   -> [:words]
+      _   -> Enum.map(parsed, &elem(&1, 0))
+    end
+
+    # Load file and do calculations
+    fileContent = File.read!(filename)
+    words = fileContent
+      |> String.split(~r{(\\n|[^\w'])+})
+      |> Enum.filter(fn x -> x != "" end)
+    chars = String.to_charlist(fileContent)
+    lines = String.split(fileContent,~r{(\r\n|\n|\r)})
+
+    # Display results
+    IO.inspect flags
+    Enum.each(flags, fn x ->
+        case x do
+            :words -> # 'w'
+                IO.puts "Words: #{Enum.count(words)}"
+            :chars -> # 'c'
+                IO.puts "Characters: #{length(chars)}"
+            :lines -> # 'l'
+                IO.puts "Lines: #{Enum.count(lines)}"
+            _ -> IO.puts ~s{Requested option "#{[x]}" does not exist}
+        end
+    end)
+
   end
 end
--- 07-word-count-escript/lib/word_count/cli.ex
+++ 07-word-count-escript/lib/word_count/cli.ex
@@ -5,6 +5,7 @@ defmodule WordCount.CLI do
         switches: [chars: nil, lines: nil, words: nil], #what should be returned
         aliases: [c: :chars, l: :lines, w: :words] # shortcut-alignment
     )
-    IO.inspect {parsed, arg, invalid}
+    # IO.inspect {parsed, arg, invalid}
+    WordCount.start(parsed, arg, invalid)
   end
 end
